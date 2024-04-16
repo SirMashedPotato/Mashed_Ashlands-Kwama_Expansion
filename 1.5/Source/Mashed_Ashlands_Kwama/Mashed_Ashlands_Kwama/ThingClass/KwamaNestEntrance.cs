930248e567bd2f52454b801430467fb7f63e64e9
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-04-16 13:46:34 +1200

    basically no progress has been made

---
+++ 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/ThingClass/KwamaNestEntrance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace Mashed_Ashlands_Kwama
+{
+    [StaticConstructorOnStartup]
+    public class KwamaNestEntrance : MapPortal
+    {
+        public override IntVec3 GetDestinationLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Map GetOtherMap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

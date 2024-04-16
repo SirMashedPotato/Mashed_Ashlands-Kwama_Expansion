eabf177171e9e8ec594551e187c07bd941d7bc29
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-04-17 10:00:01 +1200

    gaaaaaahhhhhhh

---
+++ 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/DefOf/MapGeneratorDefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Mashed_Ashlands_Kwama
+{
+    [DefOf]
+    public static class MapGeneratorDefOf
+    {
+        static MapGeneratorDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(MapGeneratorDefOf));
+        }
+        public static MapGeneratorDef Mashed_Ashlands_KwamaNest;
+    }
+}
--- 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/ThingClass/KwamaNestEntrance.cs
+++ 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/ThingClass/KwamaNestEntrance.cs
@@ -11,14 +11,30 @@ namespace Mashed_Ashlands_Kwama
     [StaticConstructorOnStartup]
     public class KwamaNestEntrance : MapPortal
     {
+        public Map kwamaNest;
+        public int kwamaNestSize = 100;
+
+        private PitGateExit pitGateExit;
+
+        public void GenerateKwamaNest()
+        {
+            kwamaNest = PocketMapUtility.GeneratePocketMap(new IntVec3(kwamaNestSize, 1, kwamaNestSize), MapGeneratorDefOf.Mashed_Ashlands_KwamaNest, null, Map);
+            pitGateExit = kwamaNest.listerThings.ThingsOfDef(ThingDefOf.PitGateExit).First() as PitGateExit;
+            //pitGateExit.pitGate = this;
+        }
+
         public override IntVec3 GetDestinationLocation()
         {
-            throw new NotImplementedException();
+            return pitGateExit?.Position ?? IntVec3.Invalid;
         }
 
         public override Map GetOtherMap()
         {
-            throw new NotImplementedException();
+            if (kwamaNest == null)
+            {
+                GenerateKwamaNest();
+            }
+            return kwamaNest;
         }
     }
 }

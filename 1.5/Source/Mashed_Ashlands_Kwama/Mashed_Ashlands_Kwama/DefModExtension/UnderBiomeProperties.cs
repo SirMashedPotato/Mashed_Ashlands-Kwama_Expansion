9e7962670dee2a1998b8a7d29971b1d6f494a50f
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-04-20 10:48:32 +1200

    custom cave plants

---
+++ 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/DefModExtension/UnderBiomeProperties.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Mashed_Ashlands_Kwama
+{
+    public class UnderBiomeProperties : DefModExtension
+    {
+        public bool increaseCavePlantWeight = true;
+        public List<BiomePlantRecord> wildPlants;
+        public List<BiomeAnimalRecord> wildAnimals;
+
+        public static UnderBiomeProperties Get(Def def)
+        {
+            return def.GetModExtension<UnderBiomeProperties>();
+        }
+    }
+}

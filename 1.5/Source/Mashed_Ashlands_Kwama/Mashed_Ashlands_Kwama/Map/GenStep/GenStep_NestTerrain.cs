18fb6f7d85800c22a25e65106f1a973ced3329f1
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-04-20 16:15:36 +1200

    polluted plants in kwama nest

--- 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/Map/GenStep/GenStep_NestTerrain.cs
+++ 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/Map/GenStep/GenStep_NestTerrain.cs
@@ -21,19 +21,19 @@ namespace Mashed_Ashlands_Kwama
                 Perlin perlin = new Perlin(0.07999999821186066, 2.0, 0.5, 6, Rand.Int, QualityMode.Medium);
                 Perlin perlin2 = new Perlin(0.1599999964237213, 2.0, 0.5, 6, Rand.Int, QualityMode.Medium);
                 MapGenFloatGrid caves = MapGenerator.Caves;
-                foreach (IntVec3 allCell in map.AllCells)
+                foreach (IntVec3 cell in map.AllCells)
                 {
-                    if (!(caves[allCell] <= 0f) && !allCell.GetTerrain(map).IsRiver)
+                    if (!(caves[cell] <= 0f) && !cell.GetTerrain(map).IsRiver)
                     {
-                        float num = (float)perlin.GetValue(allCell.x, 0.0, allCell.z);
-                        float num2 = (float)perlin2.GetValue(allCell.x, 0.0, allCell.z);
+                        float num = (float)perlin.GetValue(cell.x, 0.0, cell.z);
+                        float num2 = (float)perlin2.GetValue(cell.x, 0.0, cell.z);
                         if (num > props.waterThreshold)
                         {
-                            map.terrainGrid.SetTerrain(allCell, props.waterTerrain);
+                            map.terrainGrid.SetTerrain(cell, props.waterTerrain);
                         }
                         else if (num2 > props.gravelThreshold)
                         {
-                            map.terrainGrid.SetTerrain(allCell, props.gravelTerrain);
+                            map.terrainGrid.SetTerrain(cell, props.gravelTerrain);
                         }
                     }
                 }

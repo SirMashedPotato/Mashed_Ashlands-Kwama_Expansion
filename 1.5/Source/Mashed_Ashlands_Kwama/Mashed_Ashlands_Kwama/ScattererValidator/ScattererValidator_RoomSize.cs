7edc970ad29b2c5d7495a0e3cad4b15ac7d92dba
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2024-05-09 17:20:23 +1200

    fixing kwama burrow spawning

---
+++ 1.5/Source/Mashed_Ashlands_Kwama/Mashed_Ashlands_Kwama/ScattererValidator/ScattererValidator_RoomSize.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace Mashed_Ashlands_Kwama
+{
+    public class ScattererValidator_RoomSize : ScattererValidator
+    {
+        public int minRoomSize = 9;
+
+        public override bool Allows(IntVec3 c, Map map)
+        {
+            Room room = c.GetRoom(map);
+            return room != null && room.CellCount > minRoomSize;
+        }
+    }
+}
